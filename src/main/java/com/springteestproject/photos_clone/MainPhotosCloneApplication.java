c85de1589d9ae792d0b61efc98d545a98d9d0923
SaberHardy <samirsamado5@gmail.com>
2024-06-23 22:16:23 -0700

    get specific photo(id)

--- src/main/java/com/springteestproject/photos_clone/MainPhotosCloneApplication.java
+++ src/main/java/com/springteestproject/photos_clone/MainPhotosCloneApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainPhotosCloneApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainPhotosCloneApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MainPhotosCloneApplication.class, args);
+    }
 }
--- src/main/java/com/springteestproject/photos_clone/PhotosController.java
+++ src/main/java/com/springteestproject/photos_clone/PhotosController.java
@@ -1,15 +1,24 @@
 package com.springteestproject.photos_clone;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class PhotosController {
+    // i will replace the list of photos with a hashmap
+    private Map<String, PhotoModel> db = new HashMap<>() {{
+        put("1", new PhotoModel("1", "Hello.jpg"));
+    }};
+//    private List<PhotoModel> db = List.of(new PhotoModel("1", "Hello.jpg"));
 
-    private List<PhotoModel> db = List.of(new PhotoModel("1", "Hello.jpg"));
 
     @GetMapping("/")
     public String hello() {
@@ -17,12 +26,14 @@ public class PhotosController {
     }
 
     @GetMapping("/photos")
-    public List<PhotoModel> getPhotos() {
-        return db;
+    public Collection<PhotoModel> getPhotos() {
+        return db.values();
     }
 
     @GetMapping("/photos/{id}")
-    public PhotoModel getPhoto( String id) {
-        return db.stream().filter(photo -> photo.getId().equals(id)).findFirst().orElse(null);
+    public PhotoModel getPhoto(@PathVariable String id) {
+        PhotoModel photo = db.get(id);
+        if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        return photo;
     }
 }
