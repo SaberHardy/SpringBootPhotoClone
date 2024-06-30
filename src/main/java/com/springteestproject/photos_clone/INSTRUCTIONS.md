d0dfc9f08373b2d1fff8051635b7e571807cc94b
SaberHardy <samirsamado5@gmail.com>
2024-06-29 18:10:06 -0700

    createPhoto using js post request

--- src/main/java/com/springteestproject/photos_clone/INSTRUCTIONS.md
+++ src/main/java/com/springteestproject/photos_clone/INSTRUCTIONS.md
@@ -4,3 +4,16 @@ For delete item in javascript with an API call, run this function:
     method: "DELETE"
 })})("5");
 
+[//]: # (Create a photo)
+
+(async function createPhoto() {
+    let photo = {"fileName": "test.jpg"};
+        await fetch('http://127.0.0.1:8080/photos',{
+        method: "POST",
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
+        body: JSON.stringify(photo)
+    }).then(result => result.text()).then(text => alert(text));
+})();
--- src/main/java/com/springteestproject/photos_clone/PhotosController.java
+++ src/main/java/com/springteestproject/photos_clone/PhotosController.java
@@ -1,16 +1,10 @@
 package com.springteestproject.photos_clone;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 public class PhotosController {
@@ -42,5 +36,13 @@ public class PhotosController {
     public void deletePhoto(@PathVariable String id) {
         PhotoModel photo = db.remove(id);
         if (photo == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/photos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public PhotoModel createPhoto(@RequestBody PhotoModel photo) {
+        photo.setId(UUID.randomUUID().toString());
+        db.put(photo.getId(), photo);
+        return photo;
     }
 }
